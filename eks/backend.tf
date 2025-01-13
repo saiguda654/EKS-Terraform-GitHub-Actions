10673d4462110c0eca8fa488e3747a905a69a838
saiguda654 <132733120+saiguda654@users.noreply.github.com>
2025-01-13 17:06:42 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.10.0"
   
   backend "s3" {
-    bucket         = "saiguda654-backend"       # Use the S3 bucket name from the apply step
+    bucket         = "saiguda654-backend-1"       # Use the S3 bucket name from the apply step
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"               # Use the DynamoDB table name from the apply step
