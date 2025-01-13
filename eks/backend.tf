23c4debae065a41550a0dce64c0ac33688bef3c3
saiguda654 <132733120+saiguda654@users.noreply.github.com>
2025-01-13 17:39:23 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,4 +1,4 @@
-/*terraform {
+terraform {
   backend "s3" {
     bucket         = "saiguda654-backend-1"
     key            = "terraform.tfstate"
@@ -6,4 +6,4 @@
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
-}*\
+}
